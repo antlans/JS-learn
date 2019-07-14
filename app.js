57e1e67e4fc3249459e9fa78461214359cde946c
Ant <antchechelev@gmail.com>
2019-07-14 19:10:30 +1000

    первый коммит

---
+++ app.js
@@ -0,0 +1,44 @@
+/* document.getElementsByClassName("test")(function() {
+    var word = 'bottles';
+    var count = 3;
+
+    while (count > 0) {
+        document.write(count + ' ' + word + ' text1');
+        document.write(count + ' ' + word + ' text2');
+        document.write('text3');
+
+        count = count - 1;
+    if (count > 0) {
+        document.write(count + ' ' + word + ' text4');
+            
+        } else {
+            document.write('No more' + word + ' text5');
+        }
+    } 
+
+    return 
+});
+
+
+ */
+
+/* var name = prompt ('What is name?'); 
+var age = prompt ('How old?'); 
+
+function cry(name, age) {
+    if (age > 20) {
+        console.log(name + ' AAAAAAA');
+    } else {
+        console.log(name + ' aaaaaa');
+    }
+}
+
+cry(name, age); */
+
+function xx(param) {
+    param = 2;
+}
+var test = 1;
+xx(test);
+
+console.log(test);
