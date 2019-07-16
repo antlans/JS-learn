de95902e9174bbc4ca215a37ffcb3b55340eec06
Ant <antchechelev@gmail.com>
2019-07-16 19:25:47 +1000

    обновли код по уроку 2

--- app.js
+++ app.js
@@ -2,24 +2,36 @@
 
 let money = prompt ("Какой ваш бюджет на месяц?", 100000); 
 let time = prompt ("Введите дату в формате: YYYY-MM-DD", ""); 
-let expensesName = prompt ("Название статьи расходов в этом месяце", ""); //название обязательной статьи
-let expensesValue = prompt ("Во сколько обойдется?", "");                  //сумма обязательноЙ статьи         
-
+       
 let appData = {
-
-    budget: money,                  //получаем бюджет
+budget: money,                       //получаем бюджет
     date: time,                      //получем дату
-    expenses: {
-        [expensesName] : expensesValue
-    },                                  //обязательные расходы
+    expenses: {},                    //обязательные расходы
     optionalExpenses: null,          //необязательные расходы
     income: null,                    //дополнительные доходы
     savings: false,                  //остаток
 
 };
-
 console.log (appData);
 
-let result = (appData.budget - appData.expenses[expensesName])/30;
+for ( let i = 0; i < 2; i++) {
+    let expensesName = prompt ("Название статьи расходов в этом месяце", "");  //название обязательной статьи
+    let expensesValue = prompt ("Во сколько обойдется?", "");                  //сумма обязательноЙ статьи  
+
+    if ((typeof(expensesName)) === 'string' && 
+        (typeof(expensesName)) != null && 
+        (typeof(expensesValue)) != null && 
+        (typeof(expensesValue)) === 'number') &&
+        expensesName != '' &&
+        expensesValue != '' {
+        appData.expenses[expensesName] = expensesValue;
+    }
+   
+}
+
+
+let result = appData.budget / 30;
 console.log (result);
 alert (result);
+
+
