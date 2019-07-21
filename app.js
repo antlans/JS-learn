7982391696fde6c6b0b7e1fa9d62f90517029d49
Ant <antchechelev@gmail.com>
2019-07-21 13:57:42 +1000

    Выполнил второе задание

--- app.js
+++ app.js
@@ -1,37 +1,40 @@
 "use strict";
 
-let money = prompt ("Какой ваш бюджет на месяц?", 100000); 
+let money = +prompt ("Ваша зарплата в месяц?", 100000); 
 let time = prompt ("Введите дату в формате: YYYY-MM-DD", ""); 
        
 let appData = {
-budget: money,                       //получаем бюджет
-    date: time,                      //получем дату
+    budget: money,                   //получаем бюджет от пользователя
     expenses: {},                    //обязательные расходы
     optionalExpenses: null,          //необязательные расходы
     income: null,                    //дополнительные доходы
     savings: false,                  //остаток
-
+    date: time,                      //получем дату
 };
 console.log (appData);
 
-for ( let i = 0; i < 2; i++) {
-    let expensesName = prompt ("Название статьи расходов в этом месяце", "");  //название обязательной статьи
-    let expensesValue = prompt ("Во сколько обойдется?", "");                  //сумма обязательноЙ статьи  
+for ( let i = 0; i < 1; i++ ) {
 
-    if ((typeof(expensesName)) === 'string' && 
-        (typeof(expensesName)) != null && 
-        (typeof(expensesValue)) != null && 
-        (typeof(expensesValue)) === 'number' &&
-        expensesName != '' &&
-        expensesValue != '' ) {
+    let expensesName = prompt ("Название статьи расходов в этом месяце", "");    //название обязательной статьи
+    let expensesValue = +prompt ("Каковы затраты на эту статью расходов?", "");  //сумма обязательноЙ статьи  
+
+    if ((typeof(expensesName)) === 'string' && (typeof(expensesName)) != null && (typeof(expensesValue)) != null && (typeof(expensesValue)) === 'number' && expensesName != '' && expensesValue != '' && expensesName.length < 50 ) {
         appData.expenses[expensesName] = expensesValue;
+    } else {
+        i = i - 1;
     }
-   
 }
 
+appData.moneyPerDay = appData.budget / 30;
+console.log (appData.moneyPerDay);
+alert ("Ежедневный бюджет:" + appData.moneyPerDay);
 
-let result = appData.budget / 30;
-console.log (result);
-alert (result);
-
-
+if (appData.moneyPerDay < 500) {
+    console.log ("Low");
+} else if (appData.moneyPerDay >= 500 && appData.moneyPerDay < 2000) {
+    console.log ("Middle");
+} else if (appData.moneyPerDay >= 2000) {
+    console.log ("Rich");
+} else {
+    console.log ("Error");
+};
