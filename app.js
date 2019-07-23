6eaa96a8ac603c14492d42e5e79ebcd661e0ea55
Ant <antchechelev@gmail.com>
2019-07-23 20:17:42 +1000

    прошел урок с функциями и начала домашнее задание

--- app.js
+++ app.js
@@ -1,31 +1,44 @@
-"use strict";
+let money,
+    time;
 
-let money = +prompt ("Ваша зарплата в месяц?", 100000); 
-let time = prompt ("Введите дату в формате: YYYY-MM-DD", ""); 
-       
 let appData = {
     budget: money,                   //получаем бюджет от пользователя
     expenses: {},                    //обязательные расходы
     optionalExpenses: null,          //необязательные расходы
     income: null,                    //дополнительные доходы
-    savings: false,                  //остаток
+    savings: true,                  //остаток
     date: time,                      //получем дату
 };
-console.log (appData);
 
-for ( let i = 0; i < 1; i++ ) {
+function start() {
+    money = +prompt ("Ваша зарплата в месяц?", 100000); 
+    time = prompt ("Введите дату в формате: YYYY-MM-DD", ""); 
 
-    let expensesName = prompt ("Название статьи расходов в этом месяце", "");    //название обязательной статьи
-    let expensesValue = +prompt ("Каковы затраты на эту статью расходов?", "");  //сумма обязательноЙ статьи  
-
-    if ((typeof(expensesName)) === 'string' && (typeof(expensesName)) != null && (typeof(expensesValue)) != null && (typeof(expensesValue)) === 'number' && expensesName != '' && expensesValue != '' && expensesName.length < 50 ) {
-        appData.expenses[expensesName] = expensesValue;
-    } else {
-        i = i - 1;
+    while(isNaN(money) || money == "" || money == null) {
+        money = +prompt ("Ваша зарплата в месяц?", 100000); 
     }
 }
+start();
+       
 
-appData.moneyPerDay = appData.budget / 30;
+
+function chooseExpenses() {
+    for ( let i = 0; i < 1; i++ ) {
+
+        let expensesName = prompt ("Название статьи расходов в этом месяце", "");    //название обязательной статьи
+        let expensesValue = +prompt ("Каковы затраты на эту статью расходов?", "");  //сумма обязательноЙ статьи  
+    
+        if ((typeof(expensesName)) === 'string' && (typeof(expensesName)) != null && (typeof(expensesValue)) != null && (typeof(expensesValue)) === 'number' && expensesName != '' && expensesValue != '' && expensesName.length < 50 ) {
+            appData.expenses[expensesName] = expensesValue;
+        } else {
+            i = i - 1;
+        }
+    }
+}
+chooseExpenses();
+
+
+appData.moneyPerDay = (appData.budget / 30).toFixed(1);
 console.log (appData.moneyPerDay);
 alert ("Ежедневный бюджет:" + appData.moneyPerDay);
 
@@ -38,3 +51,16 @@ if (appData.moneyPerDay < 500) {
 } else {
     console.log ("Error");
 };
+
+
+function checkSavings() {
+    if (appData.savings == true) {
+        let save = +prompt("Какова сумма накоплений?");
+        let percent = +prompt("Под какой процент?");
+
+        appData.monthIncome = save/100/12*percent;
+
+        alert('Доход в месяц:' + appData.monthIncome);
+    }
+}
+checkSavings();
